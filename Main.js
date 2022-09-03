e6033400b3e4722395fa65ed304883702c2c20d9
Taingchhiv <106417442+Taingchhiv@users.noreply.github.com>
2022-09-04 01:13:09 +0700

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,64 @@
+let task = [
+    {name:"Doing Code", category:"school", duedate:"20-Aug-22 10:15 AM"},
+    {name:"Washing Cloth", category:"home",duedate:"20-Aug-22 8:15 PM"},
+    {name:"Play Game", category:"personal",duedate:"20-Aug-22 10:10 PM"},
+    {
+        name:"Draft email for new project",
+        category:"personal",
+        duedate:"21-Aug-22 8:00 AM",
+    },
+    {
+        name:"Play football",
+        category:"personal",
+        duedate:"21-Aug-22 1:00 PM",
+    },
+];
+
+
+
+task.forEach((tasks) =>{
+    console.log(tasks.name);
+})
+
+
+
+document.onreadystatechange = () => {
+    if (document.readyState === "complete") {
+        const taskListUL = document.querySelector(".all-task ul");
+        
+
+
+        //list all tasks
+        task.forEach((tasks) =>{
+            const taskLI = document.createElement("LI");
+            taskLI.innerHTML = `<div class="task-desc">
+            <div class="header">
+                <input type="checkbox" >
+                    <span>` + tasks.name + `</span>
+            </div>
+            <div class="desc">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-book" viewBox="0 0 16 16">
+                    <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/>
+                  </svg>
+                <span class="name">`+ tasks.category +`</span>
+                <span class="date">`+ tasks.duedate+`</span>
+            </div>
+        </div>
+        <div class="task-action">
+
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+            </svg>
+            
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
+            </svg>`
+        
+        taskListUL.appendChild(taskLI);
+        });
+    }
+  };
+
+  
+    
